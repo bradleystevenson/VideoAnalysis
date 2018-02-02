97bcba646d1d4127d154858387c52f0ed77bf37a
Bradley Stevenson <mgh219@lehigh.edu>
2018-02-02 11:59:56 -0500

    Finished the ticket

--- videoanalysis/src/main/java/videoanalysis/App.java
+++ videoanalysis/src/main/java/videoanalysis/App.java
@@ -12,14 +12,23 @@ public class App
     public static void main( String[] args ) throws Exception
     {
 	String folderName = args[0];
-	System.out.println(folderName);
 	File folder = new File(folderName);	
 	File[] listOfFiles = folder.listFiles();
 	for (File file : listOfFiles) {
 	    if (file.toString().endsWith(".png")) {
-		Image image = new Image(file.toString());
-		System.out.println(file);
+		//		Image image = new Image(file.toString());
+		//System.out.println(file);
 	    }
+	    if (file.isDirectory()) {
+		listFilesInFolder(file);
+	    }
+	}
+    }
+
+    private static void listFilesInFolder(File folder) throws Exception {
+	File[] files = folder.listFiles();
+	for (File file : files) {
+	    System.out.println(file);
 	}
     }
 }
