346d8ad5d492bab4c307cc680f5764016b4f2fcd
Bradley Stevenson <mgh219@lehigh.edu>
2018-02-08 12:08:55 -0500

    Finished the ticket

--- videoanalysis/src/main/java/videoanalysis/ScoreboardScanner.java
+++ videoanalysis/src/main/java/videoanalysis/ScoreboardScanner.java
@@ -4,24 +4,46 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ScoreboardScanner {
 
     public static void determineScoreboards(String folderName) throws Exception {
-	ArrayList<String> printStrings = new ArrayList<String>();
+	ArrayList<String> printStrings = getAlreadyStrings(folderName);
 	File folder = new File(folderName);
 	File[] files = folder.listFiles();
 	for (File file : files) {
 	    if (file.toString().endsWith(".png")) {
-		Image image = new Image(file.toString());
-		String result = ScoreboardAnswers.scoreboardScanner(image);
-		printStrings.add(file.toString() + "|" + result);
+		if (!imageAlreadyCalculated(file.toString(), printStrings)) {
+		    Image image = new Image(file.toString());
+		    String result = ScoreboardAnswers.scoreboardScanner(image);
+		    printStrings.add(file.toString() + "|" + result);
+		}
 	    }
 	}
 	printStrings(printStrings, folderName);
     }
 
+    private static boolean imageAlreadyCalculated(String imageString, ArrayList<String> printStrings) {
+	for (String string : printStrings) {
+	    if (string.contains(imageString)) {
+		return true;
+	    }	    
+	}
+	return false;
+    }
+
+    private static ArrayList<String> getAlreadyStrings(String folderName) throws Exception {
+	ArrayList<String> returnStrings = new ArrayList<String>();
+	BufferedReader in = new BufferedReader(new FileReader(folderName + "/ScoreboardScanner.txt"));
+	String readLine;
+	while ((readLine = in.readLine()) != null) {
+	    returnStrings.add(readLine);
+	}
+	return returnStrings;
+    }
+    
     private static void printStrings(ArrayList<String> printStrings, String folderName) throws Exception {
 	PrintWriter out = new PrintWriter(new FileWriter(folderName + "/ScoreboardScanner.txt"));
 	for (String string : printStrings) {
