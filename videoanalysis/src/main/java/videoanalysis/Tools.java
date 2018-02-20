2ffd45d1653e106b7d72a706455e7b73616b304a
Bradley Stevenson <mgh219@lehigh.edu>
2018-02-20 12:33:48 -0500

    Finisehd ticket

--- videoanalysis/src/main/java/videoanalysis/Tools.java
+++ videoanalysis/src/main/java/videoanalysis/Tools.java
@@ -1,7 +1,26 @@
 package videoanalysis;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Tools {
 
 
+    public static ArrayList<String> readFile(String filePath) {
+	ArrayList<String> returnStrings = new ArrayList<String>();
+	try {
+	    BufferedReader in = new BufferedReader(new FileReader(filePath));
+	    String readLine;
+	    while ((readLine = in.readLine()) != null) {
+		returnStrings.add(readLine);
+	    }
+	    in.close();
+	} catch (Exception e) {
+	    System.out.println("File it requested to read does not exist");
+	    System.exit(1);
+	}
+	return returnStrings;
+    }
 
 }
