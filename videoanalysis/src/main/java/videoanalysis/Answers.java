6481d00759a75ac0bb41f58bcb6eee1a1b749ac6
Bradley Stevenson <mgh219@lehigh.edu>
2018-03-20 15:46:22 -0400

    Finished the ticket

---
+++ videoanalysis/src/main/java/videoanalysis/Answers.java
@@ -0,0 +1,107 @@
+package videoanalysis;
+
+import java.util.ArrayList;
+import java.io.File;
+
+
+public class Answers {
+
+    private static void parseFile(String fileName, String tableName) throws Exception {
+	String folderName = getUpToLastSlash(fileName);
+	ArrayList<String> lines = Tools.readFile(fileName);
+	int maxDigits = 0;
+	for (String string : lines) {
+	    int current = maxDigits(string);
+	    if (maxDigits < current) {
+		maxDigits = current;
+	    }
+	}
+	for (String line : lines) {
+	    parseLine(line, maxDigits, folderName, tableName);
+	}
+    }
+
+    private static String getUpToLastSlash(String fileName) {
+	int inx = fileName.length() - 1;
+	char charx = fileName.charAt(inx);
+	while (charx != '/') {
+	    inx = inx - 1;
+	    charx = fileName.charAt(inx);
+	}
+	String returnString = "";
+	for (int iny = 0; iny <= inx; iny++) {
+	    returnString = returnString + fileName.charAt(iny);
+	}
+	return returnString;
+    }
+
+    private static void parseLine(String line, int maxDigits, String folderName, String tableName) throws Exception {
+	int inx = 0;
+	char charx = line.charAt(inx);
+	String firstNumberString = "";
+	while (charx != '-') {
+	    firstNumberString = firstNumberString + charx;
+	    inx++;
+	    charx = line.charAt(inx);
+	}
+	inx++;
+	charx = line.charAt(inx);
+	String secondNumberString = "";
+	while (charx != ' ') {
+	    secondNumberString = secondNumberString + charx;
+	    inx++;
+	    charx = line.charAt(inx);
+	}
+	int firstNumber = Integer.parseInt(firstNumberString);
+	int secondNumber = Integer.parseInt(secondNumberString);
+	String result = "";
+	for (inx = inx + 1; inx < line.length(); inx++) {
+	    result = result + line.charAt(inx);
+	}
+	for (inx = firstNumber; inx <= secondNumber; inx++) {
+	    String imageNumber = convertIntToString(inx, maxDigits) + ".png";
+	    String imageName = getImageName(imageNumber, folderName);
+	    Tools.insertResult(tableName, imageName, result);
+	}    
+    }
+
+    private static String getImageName(String searchString, String folderName) throws Exception {
+	File folder = new File(folderName);
+	File[] files = folder.listFiles();
+	for (File file : files) {
+	    if (file.toString().endsWith(searchString)) {
+		return file.toString();
+	    }
+	}
+	return null;
+    }
+
+    private static String convertIntToString(int number, int maxDigits) {
+	int numberPlace = (new Integer(number)).toString().length();
+	int difference = maxDigits - numberPlace;
+	String zeroString = "";
+	for (int inx = 0; inx < difference; inx++) {
+	    zeroString = zeroString + "0";
+	}
+	String numberString = (new Integer(number)).toString();
+	return zeroString + numberString;
+    }
+    
+    private static int maxDigits(String input) {
+	int maxDigits = 0;
+	int currentDigits = 0;
+	for (int inx = 0; inx < input.length(); inx++) {
+	    char charx = input.charAt(inx);
+	    if (charx >= '0' && charx <= '9') {
+		currentDigits++;
+	    } else {
+		if (maxDigits < currentDigits) {
+		    maxDigits = currentDigits;
+		}
+		currentDigits = 0;
+	    }
+	}
+	return maxDigits;
+    }
+}
+    
