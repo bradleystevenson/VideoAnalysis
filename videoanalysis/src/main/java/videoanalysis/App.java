699313e48927be7790924ae26403ea4a48367298
Bradley Stevenson <mgh219@lehigh.edu>
2018-04-12 13:33:30 -0400

    Just deleted a bunch of unneeded comments

--- videoanalysis/src/main/java/videoanalysis/App.java
+++ videoanalysis/src/main/java/videoanalysis/App.java
@@ -36,24 +36,7 @@ public class App
 	if (imageFlag) {
 	    Answers.calculateImageValues(folderName);
 	}
-	
-	/*
-	String folderName = args[0];
-	/* CompareImages.compareImages(folderName);
-	 */
-	/*
-	File folder = new File(folderName);	
-	File[] listOfFiles = folder.listFiles();
-	ColorRanges.determineColorRanges();
-	for (File file : listOfFiles) {
-	    if (file.toString().endsWith(".png")) {
-		//		Image image = new Image(file.toString());
-		//System.out.println(file);
-	    } else if (file.toString().endsWith("ScoreboardAnswers.txt")) {
-		//ScoreboardAnswers.parseFile(file.toString());
-	    }
-	}
-	*/
+
 	ScoreboardAnswers.determineScoreboardEquation();
 	Equation angle = new Equation("cameraangleresults");
 	if (angle.foundEquation()) {
